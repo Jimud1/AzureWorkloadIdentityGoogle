b8e62807590f7ec7be48483f1e8a48879617332d
Jimud1 <148221924+Jimud1@users.noreply.github.com>
2024-05-09 17:02:01 +0100

    init

---
+++ AzureWorkloadIdentityGoogleCredential/WorkloadIdenityConfig.cs
@@ -0,0 +1,24 @@
+﻿namespace AzureWorkloadIdentityGoogleCredential;
+
+public class WorkloadIdentityConfig
+{
+    /// <summary>
+    /// The application ID Uri e.g. api://app-id-url
+    /// </summary>
+    public required string AzureScope { get; set; }
+
+    /// <summary>
+    /// E.g //iam.googleapis.com/projects/{PROJECT_NUMBER}/locations/global/workloadIdentityPools/{POOL}/providers/{PROVIDER}
+    /// </summary>
+    public required string GcpStsAudience { get; set; }
+
+    /// <summary>
+    /// Google Cloud Scopes, e.g. ["https://www.googleapis.com/auth/androidpublisher", "..."]
+    /// </summary>
+    public required IList<string> GcpScopes { get; set; }
+
+    /// <summary>
+    /// email for impersonated service account
+    /// </summary>
+    public required string GcpServiceAccountEmail { get; set; }
+}

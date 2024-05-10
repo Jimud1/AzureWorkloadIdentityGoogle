abe6a0be427921781ed503398bc2208bf61e1333
Jimud1 <148221924+Jimud1@users.noreply.github.com>
2024-05-10 09:17:22 +0100

    update the naming

---
+++ AzureWorkloadIdentityGoogle/GoogleAuthConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace AzureWorkloadIdentityGoogle;
+
+public class GoogleAuthConstants
+{
+    public const string StsGrantType = "urn:ietf:params:oauth:grant-type:token-exchange";
+    public const string StsSubjectTokenType = "urn:ietf:params:oauth:token-type:jwt";
+    public const string StsScope = "https://www.googleapis.com/auth/cloud-platform";
+    public const string StsTokenType = "urn:ietf:params:oauth:token-type:access_token";
+    public const string ImpersonatedServiceAccountPrefix = "projects/-/serviceAccounts/";
+
+    public const string NoGcpScopesMessage = "No Google Cloud scopes";
+}
